17ea3c93b99eb8a90306b9283686b9b81255e7c8
Richard So <richiso899@yahoo.com>
2021-06-17 18:28:48 -0400

    radio rx/tx with inconsistency

--- arduinoRX/src/main.cpp
+++ arduinoRX/src/main.cpp
@@ -1,31 +1,39 @@
 #include <Arduino.h>
-int led = 13;
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
+SoftwareSerial lora(2, 3);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup()
 {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(led, INPUT);
+  lora.begin(115200);
+  lora.print("AT+RESET\r\n");
+  delay(20);
+  lora.print("AT+ADDRESS=102\r\n");
+  delay(20);
+  lora.print("AT+NETWORKID=5\r\n");
+  // delay(20);
+  // lora.print("AT+PARAMETER=10,7,1,7\r\n");
+  lcd.init();
+  lcd.backlight();
 }
-
-int ranVal1;
-int ranVal2;
-int ranVal3;
 
 void loop()
 {
-  ranVal1 = random(10, 20);
-  ranVal2 = random(100, 150);
-  ranVal3 = random(5);
-
-  String res = String(ranVal1) + ";" + String(ranVal2) + ";" + String(ranVal3);
-  // put your main code here, to run repeatedly:
-  Serial.println(res);
-  for (int i = 0; i < 3; i++)
+  String msg;
+  while (lora.available())
   {
-    digitalWrite(led, HIGH);
-    delay(250);
-    digitalWrite(led, LOW);
-    delay(250);
+    if (lora.available())
+    {
+      msg += String(char(lora.read()));
+    }
+  }
+  if (msg.length() > 0)
+  {
+    lcd.clear();
+    lcd.println(msg);
+    Serial.println(msg);
   }
 }
--- arduinoTX/src/main.cpp
+++ arduinoTX/src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+SoftwareSerial lora(2, 3);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  lora.begin(115200);
+  lora.print("AT+RESET\r\n");
+  delay(20);
+  lora.print("AT+ADDRESS=101\r\n");
+  delay(20);
+  lora.print("AT+NETWORKID=5\r\n");
+  // delay(20);
+  // lora.print("AT+PARAMETER=10,7,1,7\r\n");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  lora.print("AT+SEND=0,5,HELLO\r\n");
+  delay(5000);
+  lora.print("AT+SEND=0,5,WORLD\r\n");
+  delay(5000);
 }
