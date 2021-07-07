6ac604b279ea1873e5072e8407392cfa9467d99f
Richard So <richiso899@yahoo.com>
2021-07-06 23:18:38 -0400

    robust read/write, start powerup (status0)

--- lib/ADCObject/ADCObject.cpp
+++ lib/ADCObject/ADCObject.cpp
@@ -1,10 +1,45 @@
 #include "ADCObject.hpp"
 
+void ADCObject::readBytes(int ss, byte readFrom, int len, byte *data)
+{
+    digitalWrite(ss, LOW);
+    SPI.transfer(readFrom | READ);
+    // for (int i = (len - 1); i <= 0; i--)
+    for (int i = 0; i < len; i++)
+        data[i] = SPI.transfer(DC_BYTE);
+    digitalWrite(ss, HIGH);
+}
+
+byte ADCObject::readByte(int ss, byte readFrom)
+{
+    byte res;
+    digitalWrite(ss, LOW);
+    res = SPI.transfer(readFrom | READ);
+    digitalWrite(ss, HIGH);
+    return res;
+}
+
+void ADCObject::writeByte(int ss, byte writeTo, byte writeMsg)
+{
+    digitalWrite(ss, LOW);
+    SPI.transfer(writeTo);
+    SPI.transfer(writeMsg);
+    digitalWrite(ss, HIGH);
+}
+
 void ADCObject::begin(int dReadyPin, int ss1, int ss2)
 {
+    // set pins to private variables
     _dReadyPin = dReadyPin;
     _ss1 = ss1;
     _ss2 = ss2;
+
+    // pin modes for ss and dready
+    pinMode(_ss1, OUTPUT);
+    digitalWrite(_ss1, HIGH);
+    pinMode(_ss2, OUTPUT);
+    digitalWrite(_ss2, HIGH);
+    // pinMode(_dReadyPin, INPUT_PULLUP);
 
     SPISettings spiSettings(4096000, MSBFIRST, SPI_MODE3);
     SPIClass::begin();
@@ -13,5 +48,10 @@ void ADCObject::begin(int dReadyPin, int ss1, int ss2)
 
 bool ADCObject::powerUpCheck()
 {
+    bool reset = true;
+    // see when ADC A is done resetting
+    while (reset)
+        reset = readByte(_ss1, STATUS0) & 1;
+    // 
     
 }
--- lib/ADCObject/ADCObject.hpp
+++ lib/ADCObject/ADCObject.hpp
@@ -7,13 +7,19 @@
 #define DC_BYTE 0x00
 #define LOCK_BYTE 0xCA
 #define UNLOCK_BYTE 0x9C
-
+// misc variables
+#define MAX_TRY 100
 #define READ 0b100
-#define WRTE 0b000
+#define WRITE 0b000
+// registers
+#define STATUS0 (0x9 << 3)
+#define EMI_CTRL (0xE << 3)
+#define IVW_READ (0x0 << 3)
+#define V1WV_READ (0x1 << 3)
+#define V2WV_READ (0x2 << 3)
+#define ADC_CRC_READ (0x2 << 3)
 
-// read cmds
-#define STATUS0_READ (0x9 << 3 | READ)
-
+// --- IMPORTANT ---
 // This class and all its methods assumes a setup of two
 // ADE7912's clocked with an external 4.096 MHz crystal
 class ADCObject : public SPIClass
@@ -21,12 +27,16 @@ class ADCObject : public SPIClass
 private:
     int _dReadyPin, _ss1, _ss2;
 
+    void readBytes(int ss, byte readFrom, int len, byte *data);
+
+    byte readByte(int ss, byte readFrom);
+
+    void writeByte(int ss, byte writeTo, byte writeMsg);
+
 public:
     // start ADC SPI with optimal settings
     void begin(int dReadyPin, int ss1, int ss2);
 
     // run power-up procedure and return if complete
     bool powerUpCheck();
-
-
 };
