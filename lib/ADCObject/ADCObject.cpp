27f25b158dea980cd4cc3f5d1d5614efb726e35b
Richard So <richiso899@yahoo.com>
2021-07-09 00:59:00 -0400

    complete with setup, work on data conversion

--- lib/ADCObject/ADCObject.cpp
+++ lib/ADCObject/ADCObject.cpp
@@ -1,57 +1,114 @@
 #include "ADCObject.hpp"
 
-void ADCObject::readBytes(int ss, byte readFrom, int len, byte *data)
-{
-    digitalWrite(ss, LOW);
-    SPI.transfer(readFrom | READ);
-    // for (int i = (len - 1); i <= 0; i--)
-    for (int i = 0; i < len; i++)
-        data[i] = SPI.transfer(DC_BYTE);
-    digitalWrite(ss, HIGH);
-}
-
 byte ADCObject::readByte(int ss, byte readFrom)
 {
     byte res;
     digitalWrite(ss, LOW);
-    res = SPI.transfer(readFrom | READ);
+    res = transfer(readFrom | READ);
     digitalWrite(ss, HIGH);
     return res;
+}
+
+void ADCObject::readBytes(int ss, byte readFrom, int len, byte *data)
+{
+    digitalWrite(ss, LOW);
+    transfer(readFrom | READ);
+    // for (int i = (len - 1); i <= 0; i--)
+    for (int i = 0; i < len; i++)
+        data[i] = transfer(DC_BYTE);
+    digitalWrite(ss, HIGH);
 }
 
 void ADCObject::writeByte(int ss, byte writeTo, byte writeMsg)
 {
     digitalWrite(ss, LOW);
-    SPI.transfer(writeTo);
-    SPI.transfer(writeMsg);
+    transfer(writeTo);
+    transfer(writeMsg);
     digitalWrite(ss, HIGH);
 }
 
-void ADCObject::begin(int dReadyPin, int ss1, int ss2)
+void ADCObject::init(int dReadyPin, int ss1, int ss2)
 {
     // set pins to private variables
     _dReadyPin = dReadyPin;
     _ss1 = ss1;
     _ss2 = ss2;
+    // _spiSettings = &SPISettings(4096000, MSBFIRST, SPI_MODE3);
+    _spiSettings = &SPISettings(2500000, MSBFIRST, SPI_MODE3);
 
-    // pin modes for ss and dready
+    // pin modes for ss
     pinMode(_ss1, OUTPUT);
     digitalWrite(_ss1, HIGH);
     pinMode(_ss2, OUTPUT);
     digitalWrite(_ss2, HIGH);
-    // pinMode(_dReadyPin, INPUT_PULLUP);
-
-    SPISettings spiSettings(4096000, MSBFIRST, SPI_MODE3);
-    SPIClass::begin();
-    SPIClass::beginTransaction(spiSettings);
+    begin();
 }
 
-bool ADCObject::powerUpCheck()
+void ADCObject::powerUp()
 {
-    bool reset = true;
+    beginTransaction(*_spiSettings);
+
+    bool resetA = true;
+    bool resetB = true;
     // see when ADC A is done resetting
-    while (reset)
-        reset = readByte(_ss1, STATUS0) & 1;
-    // 
+    while (resetA)
+        resetA = readByte(_ss1, STATUS0) & 1;
+
+    // unlock config register
+    writeByte(_ss1, LOCK_REG, UNLOCK_BYTE);
+
+    // set config for ADC A to have CLKOUT functionality
+    writeByte(_ss1, CONFIG, 0b00110001);
+
+    // set EMI_CTRL for ADC A
+    writeByte(_ss1, EMI_CTRL, EMI_A);
+
+    // repeat for ADC B
+    while (resetB)
+        resetB = readByte(_ss2, STATUS0) & 1;
+    writeByte(_ss2, LOCK_REG, UNLOCK_BYTE);
+    writeByte(_ss2, CONFIG, 0b00110000);
+    writeByte(_ss2, EMI_CTRL, EMI_B);
+
+    // execute SYNC_SNAP for both ADCs
+    writeByte(_ss1, SYNC_SNAP, SYNC);
+    writeByte(_ss2, SYNC_SNAP, SYNC);
+
+    // lock registers for both ADCs
+    writeByte(_ss1, LOCK_REG, LOCK_BYTE);
+    writeByte(_ss2, LOCK_REG, LOCK_BYTE);
+
+    endTransaction();
+}
+
+void ADCObject::burstReadData(double *ADCData)
+{
+    beginTransaction(*_spiSettings);
+    byte dataA[6];
+    byte dataB[6];
+    double resA[2];
+    double resB[2];
+
+    // read IWV and V1 from both ADCs
+    readBytes(_ss1, IWV, 6, dataA);
+    readBytes(_ss2, IWV, 6, dataB);
+
+    translateDataBytes(dataA, resA, 249000, 1);
+    translateDataBytes(dataB, resB, 100, 1);
+
+    endTransaction();
+}
+
+void ADCObject::translateDataBytes(byte *data, double *res, uint64_t r1, uint64_t r2)
+{
+    int64_t iwv_res = 0;
+    int64_t v1_res = 0;
+
+    for (int i = 0; i < 3; i++)
+        iwv_res = (iwv_res << 8) | data[i];
+    for (int i = 3; i < 6; i++)
+        v1_res = (v1_res << 8) | data[i];
     
+    res[0] = iwv_res;
+
 }
--- lib/ADCObject/ADCObject.hpp
+++ lib/ADCObject/ADCObject.hpp
@@ -7,17 +7,24 @@
 #define DC_BYTE 0x00
 #define LOCK_BYTE 0xCA
 #define UNLOCK_BYTE 0x9C
+#define EMI_A 0x55
+#define EMI_B 0xAA
+#define SYNC 0x01
+#define SNAP 0x02
 // misc variables
 #define MAX_TRY 100
 #define READ 0b100
-#define WRITE 0b000
+// #define WRITE 0b000
 // registers
 #define STATUS0 (0x9 << 3)
 #define EMI_CTRL (0xE << 3)
-#define IVW_READ (0x0 << 3)
-#define V1WV_READ (0x1 << 3)
-#define V2WV_READ (0x2 << 3)
-#define ADC_CRC_READ (0x2 << 3)
+#define CONFIG (0x8 << 3)
+#define LOCK_REG (0xA << 3)
+#define SYNC_SNAP (0xB << 3)
+#define IWV (0x0 << 3)
+#define V1WV (0x1 << 3)
+#define V2WV (0x2 << 3)
+#define ADC_CRC (0x4 << 3)
 
 // --- IMPORTANT ---
 // This class and all its methods assumes a setup of two
@@ -26,17 +33,27 @@ class ADCObject : public SPIClass
 {
 private:
     int _dReadyPin, _ss1, _ss2;
+    SPISettings *_spiSettings;
 
-    void readBytes(int ss, byte readFrom, int len, byte *data);
-
+    // read and return one byte from a register
     byte readByte(int ss, byte readFrom);
 
+    // read bytes from a register to a byte array
+    void readBytes(int ss, byte readFrom, int len, byte *data);
+
+    // write a byte onto a register
     void writeByte(int ss, byte writeTo, byte writeMsg);
 
 public:
     // start ADC SPI with optimal settings
-    void begin(int dReadyPin, int ss1, int ss2);
+    void init(int dReadyPin, int ss1, int ss2);
 
     // run power-up procedure and return if complete
-    bool powerUpCheck();
+    void powerUp();
+
+    // get values of data
+    void burstReadData(double *ADCData);
+
+    // translate an array of bytes into unsigned long
+    void translateDataBytes(byte *data, double *res, uint64_t r1, uint64_t r2);
 };
